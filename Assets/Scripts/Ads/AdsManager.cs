5062c7a2bbc965f6d8c2224058a1f1f995f24fdf
Agustin Pontura <pontura@gmail.com>
2026-05-10 10:33:14 -0300

    clean up

--- Assets/Scripts/Ads/AdsManager.cs
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -18,31 +18,31 @@ namespace Fulbo.Admob
         public void Start()
         {
 
-#if UNITY_IOS
-        rewardedAdUnitId = "5d7209eed1474617";
-#endif
-            if (DB.DBManager.Instance.versionMode == DB.DBManager.versionModes.PROD)
-            {
-                MaxSdkCallbacks.OnSdkInitializedEvent += (MaxSdkBase.SdkConfiguration sdkConfiguration) =>
-                {
-                    InitializeRewardedAds();
-                    InitializeIntersititialAds();
-                };
-            }
-            else
-            {
-                MaxSdkCallbacks.OnSdkInitializedEvent += (MaxSdkBase.SdkConfiguration sdkConfiguration) =>
-                {
-                    // AppLovin SDK is initialized, start loading ads
-                    InitializeRewardedAds();
-                    InitializeIntersititialAds();
-                    MaxSdk.ShowMediationDebugger();
-                };
-            }
+//#if UNITY_IOS
+//        rewardedAdUnitId = "5d7209eed1474617";
+//#endif
+//            if (DB.DBManager.Instance.versionMode == DB.DBManager.versionModes.PROD)
+//            {
+//                MaxSdkCallbacks.OnSdkInitializedEvent += (MaxSdkBase.SdkConfiguration sdkConfiguration) =>
+//                {
+//                    InitializeRewardedAds();
+//                    InitializeIntersititialAds();
+//                };
+//            }
+//            else
+//            {
+//                MaxSdkCallbacks.OnSdkInitializedEvent += (MaxSdkBase.SdkConfiguration sdkConfiguration) =>
+//                {
+//                    // AppLovin SDK is initialized, start loading ads
+//                    InitializeRewardedAds();
+//                    InitializeIntersititialAds();
+//                    MaxSdk.ShowMediationDebugger();
+//                };
+//            }
 
-            MaxSdk.SetSdkKey(SDK);
-            //MaxSdk.SetUserId(USER_ID);
-            MaxSdk.InitializeSdk();
+//            MaxSdk.SetSdkKey(SDK);
+//            //MaxSdk.SetUserId(USER_ID);
+//            MaxSdk.InitializeSdk();
 
             Events.AdsWatchVideo += AdsWatchVideo;
             Events.AdsWatchInterstitial += AdsWatchInterstitial;
@@ -66,32 +66,32 @@ namespace Fulbo.Admob
         }
         private void ShowRewardedAd()
         {
-            if (MaxSdk.IsRewardedAdReady(rewardedAdUnitId))
-            {
-                Debug.Log("ADS AdsWatchVideo");
-                MaxSdk.ShowRewardedAd(rewardedAdUnitId);
-            } else {
-                Events.OnPopup(Data.Instance.texts.Get("no_available_ads"), () => OnReady(false));
-            }
+            //if (MaxSdk.IsRewardedAdReady(rewardedAdUnitId))
+            //{
+            //    Debug.Log("ADS AdsWatchVideo");
+            //    MaxSdk.ShowRewardedAd(rewardedAdUnitId);
+            //} else {
+            //    Events.OnPopup(Data.Instance.texts.Get("no_available_ads"), () => OnReady(false));
+            //}
         }
         private void ShowInterstitialAd()
         {
-            if (MaxSdk.IsInterstitialReady(interstitialAdUnitId))
-            {
-                Debug.Log("ADS Interstitial");
-                MaxSdk.ShowInterstitial(interstitialAdUnitId);
-            }
+            //if (MaxSdk.IsInterstitialReady(interstitialAdUnitId))
+            //{
+            //    Debug.Log("ADS Interstitial");
+            //    MaxSdk.ShowInterstitial(interstitialAdUnitId);
+            //}
         }
 
         public void InitializeIntersititialAds()
         {
             // Attach callback
-            MaxSdkCallbacks.Interstitial.OnAdLoadedEvent += OnInterstitialLoadedEvent;
-            MaxSdkCallbacks.Interstitial.OnAdLoadFailedEvent += OnInterstitialLoadFailedEvent;
-            MaxSdkCallbacks.Interstitial.OnAdDisplayedEvent += OnInterstitialDisplayedEvent;
-            MaxSdkCallbacks.Interstitial.OnAdClickedEvent += OnInterstitialClickedEvent;
-            MaxSdkCallbacks.Interstitial.OnAdHiddenEvent += OnInterstitialHiddenEvent;
-            MaxSdkCallbacks.Interstitial.OnAdDisplayFailedEvent += OnInterstitialAdFailedToDisplayEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdLoadedEvent += OnInterstitialLoadedEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdLoadFailedEvent += OnInterstitialLoadFailedEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdDisplayedEvent += OnInterstitialDisplayedEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdClickedEvent += OnInterstitialClickedEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdHiddenEvent += OnInterstitialHiddenEvent;
+            //MaxSdkCallbacks.Interstitial.OnAdDisplayFailedEvent += OnInterstitialAdFailedToDisplayEvent;
 
             // Load the first rewarded ad
             LoadInterstitial();
@@ -100,14 +100,14 @@ namespace Fulbo.Admob
         public void InitializeRewardedAds()
         {
             // Attach callback
-            MaxSdkCallbacks.Rewarded.OnAdLoadedEvent += OnRewardedAdLoadedEvent;
-            MaxSdkCallbacks.Rewarded.OnAdLoadFailedEvent += OnRewardedAdLoadFailedEvent;
-            MaxSdkCallbacks.Rewarded.OnAdDisplayedEvent += OnRewardedAdDisplayedEvent;
-            MaxSdkCallbacks.Rewarded.OnAdClickedEvent += OnRewardedAdClickedEvent;
-            MaxSdkCallbacks.Rewarded.OnAdRevenuePaidEvent += OnRewardedAdRevenuePaidEvent;
-            MaxSdkCallbacks.Rewarded.OnAdHiddenEvent += OnRewardedAdHiddenEvent;
-            MaxSdkCallbacks.Rewarded.OnAdDisplayFailedEvent += OnRewardedAdFailedToDisplayEvent;
-            MaxSdkCallbacks.Rewarded.OnAdReceivedRewardEvent += OnRewardedAdReceivedRewardEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdLoadedEvent += OnRewardedAdLoadedEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdLoadFailedEvent += OnRewardedAdLoadFailedEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdDisplayedEvent += OnRewardedAdDisplayedEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdClickedEvent += OnRewardedAdClickedEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdRevenuePaidEvent += OnRewardedAdRevenuePaidEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdHiddenEvent += OnRewardedAdHiddenEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdDisplayFailedEvent += OnRewardedAdFailedToDisplayEvent;
+            //MaxSdkCallbacks.Rewarded.OnAdReceivedRewardEvent += OnRewardedAdReceivedRewardEvent;
 
             // Load the first rewarded ad
             LoadRewardedAd();
@@ -115,85 +115,83 @@ namespace Fulbo.Admob
 
         private void LoadRewardedAd()
         {
-            MaxSdk.LoadRewardedAd(rewardedAdUnitId);
+        //    MaxSdk.LoadRewardedAd(rewardedAdUnitId);
         }
         private void LoadInterstitial()
         {
-            MaxSdk.LoadInterstitial(interstitialAdUnitId);
+         //   MaxSdk.LoadInterstitial(interstitialAdUnitId);
         }
 
-        private void OnRewardedAdLoadedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
-        {
-            Debug.Log(" Rewarded ad is ready for you to show. MaxSdk.IsRewardedAdReady(adUnitId) now returns 'true'.");
+        //private void OnRewardedAdLoadedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    Debug.Log(" Rewarded ad is ready for you to show. MaxSdk.IsRewardedAdReady(adUnitId) now returns 'true'.");
 
-            // Reset retry attempt
-            retryAttempt = 0;
-        }
+        //    // Reset retry attempt
+        //    retryAttempt = 0;
+        //}
 
-        private void OnRewardedAdLoadFailedEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
-        {
-            Debug.Log("OnRewardedAdLoadFailedEvent retryAttempt: " + retryAttempt);
+        //private void OnRewardedAdLoadFailedEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
+        //{
+        //    Debug.Log("OnRewardedAdLoadFailedEvent retryAttempt: " + retryAttempt);
 
-            // Rewarded ad failed to load 
-            // AppLovin recommends that you retry with exponentially higher delays, up to a maximum delay (in this case 64 seconds).
+        //    // Rewarded ad failed to load 
+        //    // AppLovin recommends that you retry with exponentially higher delays, up to a maximum delay (in this case 64 seconds).
 
-            retryAttempt++;
+        //    retryAttempt++;
 
-            if (retryAttempt > 6) {
-                if (OnReady != null)
-                    OnReady(false);
-            } else {
-                double retryDelay = Math.Pow(2, Math.Min(6, retryAttempt));
-                Invoke("LoadRewardedAd", (float)retryDelay);
-            }
-        }
+        //    if (retryAttempt > 6) {
+        //        if (OnReady != null)
+        //            OnReady(false);
+        //    } else {
+        //        double retryDelay = Math.Pow(2, Math.Min(6, retryAttempt));
+        //        Invoke("LoadRewardedAd", (float)retryDelay);
+        //    }
+        //}
 
-        private void OnRewardedAdDisplayedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
+        //private void OnRewardedAdDisplayedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
 
-            Debug.Log("OnRewardedAdDisplayedEvent retryAttempt: " + adInfo);
+        //    Debug.Log("OnRewardedAdDisplayedEvent retryAttempt: " + adInfo);
 
-        }
+        //}
 
-        private void OnRewardedAdFailedToDisplayEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
-        {
-            Debug.Log("LoadRewardedAd adUnitId: " + adUnitId + " errorInfo: " + errorInfo.Message);
-            // Rewarded ad failed to display. AppLovin recommends that you load the next ad.
-            LoadRewardedAd();
-        }
+        //private void OnRewardedAdFailedToDisplayEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    Debug.Log("LoadRewardedAd adUnitId: " + adUnitId + " errorInfo: " + errorInfo.Message);
+        //    // Rewarded ad failed to display. AppLovin recommends that you load the next ad.
+        //    LoadRewardedAd();
+        //}
 
-        private void OnRewardedAdClickedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
+        //private void OnRewardedAdClickedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
 
-            Debug.Log("OnRewardedAdClickedEvent: " + adInfo);
-        }
+        //    Debug.Log("OnRewardedAdClickedEvent: " + adInfo);
+        //}
 
-        private void OnRewardedAdHiddenEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
-        {
-            // Rewarded ad is hidden. Pre-load the next ad
-            LoadRewardedAd();
-        }
-        private void OnRewardedAdReceivedRewardEvent(string adUnitId, MaxSdk.Reward reward, MaxSdkBase.AdInfo adInfo)
-        {
-            Debug.Log("OnRewardedAd Received Reward Event adUnitId: " + adUnitId + " Label: " + reward.Label + " Amount:" + reward.Amount);
-            OnReady(true);
+        //private void OnRewardedAdHiddenEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    // Rewarded ad is hidden. Pre-load the next ad
+        //    LoadRewardedAd();
+        //}
+        //private void OnRewardedAdReceivedRewardEvent(string adUnitId, MaxSdk.Reward reward, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    Debug.Log("OnRewardedAd Received Reward Event adUnitId: " + adUnitId + " Label: " + reward.Label + " Amount:" + reward.Amount);
+        //    OnReady(true);
 
-            //Analytics
-            //Dictionary<string, object> param = new Dictionary<string, object>();
-            //param["adType"] = reward.Label;
-            //param["adID"] = adUnitId;
+        //    //Analytics
+        //    //Dictionary<string, object> param = new Dictionary<string, object>();
+        //    //param["adType"] = reward.Label;
+        //    //param["adID"] = adUnitId;
 
-            //Events.OnTrack("OnAdDone", param);
+        //    //Events.OnTrack("OnAdDone", param);
 
-            // The rewarded ad displayed and the user should receive the reward.
-        }
+        //    // The rewarded ad displayed and the user should receive the reward.
+        //}
 
-        private void OnRewardedAdRevenuePaidEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
-        {
-            Debug.Log("Load RewardedAd RevenuePaidEvent: " + adUnitId + " adInfo: " + adInfo);
-            //OnReady(true);
-            // Ad revenue paid. Use this callback to track user revenue.
-        }
-
-
+        //private void OnRewardedAdRevenuePaidEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    Debug.Log("Load RewardedAd RevenuePaidEvent: " + adUnitId + " adInfo: " + adInfo);
+        //    //OnReady(true);
+        //    // Ad revenue paid. Use this callback to track user revenue.
+        //}
 
 
 
@@ -202,42 +200,44 @@ namespace Fulbo.Admob
 
 
 
-        private void OnInterstitialLoadedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
-        {
-            // Interstitial ad is ready for you to show. MaxSdk.IsInterstitialReady(adUnitId) now returns 'true'
 
-            // Reset retry attempt
-            retryAttempt = 0;
-        }
 
-        private void OnInterstitialLoadFailedEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
-        {
-            // Interstitial ad failed to load 
-            // AppLovin recommends that you retry with exponentially higher delays, up to a maximum delay (in this case 64 seconds)
+        //private void OnInterstitialLoadedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    // Interstitial ad is ready for you to show. MaxSdk.IsInterstitialReady(adUnitId) now returns 'true'
 
-            retryAttempt++;
-            double retryDelay = Math.Pow(2, Math.Min(6, retryAttempt));
+        //    // Reset retry attempt
+        //    retryAttempt = 0;
+        //}
 
-            Invoke("LoadInterstitial", (float)retryDelay);
-        }
+        //private void OnInterstitialLoadFailedEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
+        //{
+        //    // Interstitial ad failed to load 
+        //    // AppLovin recommends that you retry with exponentially higher delays, up to a maximum delay (in this case 64 seconds)
 
-        private void OnInterstitialDisplayedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
-            OnReady(true);
-        }
+        //    retryAttempt++;
+        //    double retryDelay = Math.Pow(2, Math.Min(6, retryAttempt));
 
-        private void OnInterstitialAdFailedToDisplayEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
-        {
-            // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
-            LoadInterstitial();
-        }
+        //    Invoke("LoadInterstitial", (float)retryDelay);
+        //}
 
-        private void OnInterstitialClickedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) { }
+        //private void OnInterstitialDisplayedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) {
+        //    OnReady(true);
+        //}
 
-        private void OnInterstitialHiddenEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
-        {
-            // Interstitial ad is hidden. Pre-load the next ad.
-            LoadInterstitial();
-        }
+        //private void OnInterstitialAdFailedToDisplayEvent(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+        //    LoadInterstitial();
+        //}
+
+        //private void OnInterstitialClickedEvent(string adUnitId, MaxSdkBase.AdInfo adInfo) { }
+
+        //private void OnInterstitialHiddenEvent(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        //{
+        //    // Interstitial ad is hidden. Pre-load the next ad.
+        //    LoadInterstitial();
+        //}
 
     }
 
